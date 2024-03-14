53177b3f0c86fb546840628a8fcd5e63e2b83317
KrishBeanSir <160157579+KrishBeanSir@users.noreply.github.com>
2024-03-14 16:29:40 +0545

    first:

---
+++ main.py
@@ -0,0 +1,343 @@
+
+
+# data={
+# 'name':'ram',
+# 'age':20,
+# 'address' : [
+#     {
+#         'country': 'napal',
+#         'city':{
+#             'capital':'Kathmandu',
+#             'province':'bagmati'
+
+#         }
+#     }
+# ],
+
+
+# print(10+5)
+# print(2*2)
+# print(2**2)
+# print(10%3)
+# print(19/5)
+# print(19//5)
+
+# name="Ram"
+# print('a'in name)
+# print('b'in name)
+# print('a'not in name)
+
+# a=6
+# b=7
+# c=a
+# d=8
+# print(a is b)
+# print(a==b)
+# print(a is not b)
+# print(a is c)
+
+
+
+# a=5
+# b=7
+# if a>b:
+#     print("a is greater then b")
+# else:
+#     print("b is greater then a")
+
+# a=5
+# b=7
+# c=8
+# if a>b and a<c:
+#     print ("A")
+# elif b>a and b>c:
+#     print("B")
+# else:
+#     print("C")
+
+# a=5
+# b=7
+# c=8
+# d=9
+
+# if a>b and a>c and a>d:
+#      print("a")
+# elif b>a and b>c and b>d:
+#      print("b")
+# elif c>a and c>b and c>d:
+#      print("c")
+# else:
+#      print("d")
+
+
+# product={
+#     "name" : "Laptop"
+#     "price" : 100000,
+#     "quantity" : 10
+# }
+#  a=int(input("enter number of laptop"))
+
+# users=[
+#     ['admin','admin002'],
+#     ['ram','ram002']
+# ]
+
+# input_username = input("Enter username: ")
+# input_password = input("Enter password: ")
+# if [input_username, input_password] in users:
+#      print("welcome")
+# else:
+#      print("Invalid username or password.")
+
+# users=[
+#     {'username':"admin",'password':"admin002"},
+#     {'username':"ram",'password':"ram002"}
+# ]
+# input_username = input("Enter username: ")
+# input_password = input("Enter password: ")
+
+# if any(user['username'] == input_username and user['password'] == input_password for user in users):
+#     print("Welcome!")
+# else:
+#     print("Invalid username or password.")
+
+# data=[1,2,57]
+# for num in data:
+#       print(num)
+# x=1
+# while x<=10:
+#     print(x)
+#     x+=1
+# x=10
+# while x>=1:
+#       print(x)
+#       x-=1
+
+
+# x = 1
+# while x <= 10:
+#     if x % 2 == 0:
+#         print(x)
+#     x += 1
+
+# x = 1
+# while x <= 10:
+#     if x % 2 == 1:
+#         print(x)
+#     x += 1
+
+# x =["apple","ball","cat","dog"]
+# print(len(x))
+
+
+# data = []
+# x = 1
+
+# while x <= 5:
+#     name = input("Enter name: ")
+#     data.append(name)
+#     x += 1
+
+# for a in data:
+#     print(a)
+  
+# a=int(input("Enter marks in Nepali :"))
+# b=int(input("Enter marks in English :"))
+# c=int(input("Enter marks in Math :"))
+# d=int(input("Enter marks in Social :"))
+# e=int(input("Enter marks in Science :"))
+# Total_Marks=a+b+c+d+e
+# print("Total Marks:",Total_Marks)
+# Average_Marks=(Total_Marks) /5
+# print("Average Marks:",Average_Marks)
+
+# a, b, *c, d = [1, 2, 3, 4, 5, 6, 7]
+# print(c)
+
+
+# news=[
+#     {'title':'first news','description':'this is the first news'},
+#     {'title':'first news','description':'this is the first news'},
+#     {'title':'first news','description':'this is the first news'},
+# ]
+# for n in news:
+#     print(n['title'])
+
+# numbers=[
+#     [1,2,3,4,5],
+#     [6,7,8,9,10],
+#     [11,12,13,14,15]
+# ]
+
+# for row in numbers:
+#     for col in row:
+#         if col%2==0:
+#            print(col)
+
+
+# for sublist in numbers:
+#     for number in sublist:
+#         if number % 2 == 0:
+#             print(number)
+
+# users={
+#     'name':'ram',
+#     'address':{
+#         'city':'ktm',
+#         'email':'ram@gmail.com'
+#     }
+# }
+# print(users['address']['email'])
+# print(users['address']['city'])
+# print(users['name'])
+
+
+# print("\"we are learning python programming\"")
+# print('\'we are learning python programming\'')
+# print("\"mother's\"")
+# print('\'mother\'s\'')
+
+# name='ram'
+# address='ktm'
+# print("my name is", name)
+# print("my name is {} address {}".format(name,address))
+# print(f"my name is {name} address {address}")
+# print("my n ame is % address %s"%(name,address))
+
+# course ="we are learning Python programmming"
+# upper
+# lower
+# title
+# capitalize
+# course.strip
+# course.lstrip
+# course.rstrip
+
+# first_name ="         rAm"
+# last_name ="sHrMa"
+# first_name =first_name.strip()
+# last_name =last_name.strip()
+# print
+
+# coursecourse ="we are learning Python programmming"
+# print(learning)
+# name= Ram Sharma
+# print(RSM)
+
+
+# data=["sophia", "ram", "hari"]
+# # data.append("laxmi")
+# # data.pop()
+# # data.remove("ram")
+# data.insert(2, "hari")
+# print(data)
+
+# data types---- list,tuple- set - dis-- opre(otho,assigment,copa,logical, bitwise, id,membership)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def user():
+#     print("hello user")
+    
+# user()
+
+# def my_print(data):
+#     print(data)
+# my_print("hello world")
+
+# def add(x,y):
+#     print(x+y)
+
+# add(5,6)
+# add(10,20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# p= input("enter p:")
+# t= input("enter t:")
+# r= input("enter r:")
+# p=int(p)
+# t=int(t)
+# r=int(r)
+# print(p*t*r/100)
+
+
+# what is python?
+# what is data types
+# what is variable
+# variable rules
+
+
+# what is operator
+
+
+
+
+
+
+
+
+
+
+
+
